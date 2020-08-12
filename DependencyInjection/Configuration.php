fd2683d5477ced68bba2de651454eae952bff9dd
Yansell Rivas Diaz <yrivas@syneteksolutions.com>
2020-08-12 16:14:48 +0000

    feat(SyncBundle) Symfony 4 support
     * Fix dependency injection Root Node compatibility with symfony 4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,12 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('nti_sync');
+        $treeBuilder = new TreeBuilder('nti_sync');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root('nti_sync');
+        }
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
