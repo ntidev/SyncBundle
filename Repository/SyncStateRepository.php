5f32d737d7006c41c7a3bb2e57754015b0789ae5
root <root@SYNNTIDEV20.SynetekSolutions.local>
2022-10-13 10:37:35 -0400

    updgrade for Symfony 5

--- Repository/SyncStateRepository.php
+++ Repository/SyncStateRepository.php
@@ -3,7 +3,7 @@
 namespace NTI\SyncBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use NTI\SyncBundle\Entity\SyncState;
 
 /**
